ca03cc05ca91060ed86c5f23570c3bd22f352ab4
caluhm <calum.jarvo@gmail.com>
2023-06-28 19:52:18 +0100

    current nav item works dynamically now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { RxHamburgerMenu } from 'react-icons/rx'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
-    const [selected, setSelected] = useState('Home')
+    const pathname = usePathname()
+
+    const navLinks = [
+        { name: 'Home', link: '/' },
+        { name: 'Reaction', link: '/games/reaction' },
+        { name: 'Sequence', link: '/games/sequence' },
+        { name: 'Typing', link: '/games/typing' },
+    ];
+
   return (
     <nav className='h-[5rem] w-full bg-white py-2.5'>
         <div className='h-full flex flex-row items-center justify-between sm:px-18 px-8'>
@@ -15,18 +24,11 @@ const Navbar = () => {
             </Link>
             </div>
             <ul className='hidden sm:flex flex-row items-center gap-10 text-gray-950 text-md font-semibold tracking-wide'>
-            <li className={` ${selected === 'Home' ? 'text-[#2B87D1] hover:text-[#78baf0]' : 'hover:text-gray-500' } transition-colors ease-in-out duration-200`} onClick={() => setSelected('Home')}>
-                <Link href='/'>Home</Link>
-            </li>
-            <li className={` ${selected === 'Reaction' ? 'text-[#2B87D1] hover:text-[#78baf0]' : 'hover:text-gray-500' } transition-colors ease-in-out duration-200`} onClick={() => setSelected('Reaction')}>
-                <Link href='/games/reaction'>Reaction</Link>
-            </li>
-            <li className={` ${selected === 'Sequence' ? 'text-[#2B87D1] hover:text-[#78baf0]' : 'hover:text-gray-500' } transition-colors ease-in-out duration-200`} onClick={() => setSelected('Sequence')}>
-                <Link href='/games/sequence'>Sequence</Link>
-            </li>
-            <li className={` ${selected === 'Typing' ? 'text-[#2B87D1] hover:text-[#78baf0]' : 'hover:text-gray-500' } transition-colors ease-in-out duration-200`} onClick={() => setSelected('Typing')}>
-                <Link href='/games/typing'>Typing</Link>
-            </li>
+                {navLinks.map((item) => (
+                    <li key={item.name} className={pathname === item.link ? 'text-[#2B87D1] hover:text-[#78baf0] transition-colors ease-in-out duration-200' : 'hover:text-gray-400 transition-colors ease-in-out duration-200'}>
+                        <Link href={item.link}>{item.name}</Link>
+                    </li>
+                ))}
             </ul>
             <div className='sm:hidden flex items-center justify-center text-gray-950'>
                 <RxHamburgerMenu size={32} />
